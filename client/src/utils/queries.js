4dbb8de6d497ed16cee47e549a54ead7f06a46de
jjakobsons87 <98094445+jjakobsons87@users.noreply.github.com>
2022-07-06 09:57:51 -0400

    update queries

---
+++ client/src/utils/queries.js
@@ -0,0 +1,16 @@
+import { gql } from '@apollo/client';
+
+export const GET_ME = gql `
+    me {
+        _id
+        username
+        email
+        savedBooks {
+            title
+            authors
+            description
+            image
+            link
+        }
+    }
+`;
